478a50b3a0618528278a7c4f10219f4295234072
gayan68 <gayan68@yahoo.com>
2025-11-14 12:20:56 +0100

    sh

--- Riksakivet/split_dataset.py
+++ Riksakivet/split_dataset.py
@@ -2,7 +2,7 @@ from datasets import load_dataset
 import os
 
 
-folder ="svea_hovratt_lines"
+folder ="goteborgs_poliskammare_fore_1900_lines"
 
 dataset  = load_dataset(
     f"Riksarkivet/{folder}",
