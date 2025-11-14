dee189bca34b1f55ca111efba48363eeeaca38da
gayan68 <gayan68@yahoo.com>
2025-11-14 14:52:32 +0100

    sh

---
+++ Riksakivet/test_sample.py
@@ -0,0 +1,11 @@
+from datasets import load_from_disk
+
+folder ="svea_hovratt_lines"
+
+train_path = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/train/{folder}"
+train_ds = load_from_disk(train_path)
+
+print(train_ds)
+
+# print first sample
+print(train_ds[0])
