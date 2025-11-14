eafd9c3356b25937891b244ba67098fccf3510b3
gayan68 <gayan68@yahoo.com>
2025-11-14 15:05:32 +0100

    sh

--- Riksakivet/download_datasets.py
+++ Riksakivet/download_datasets.py
@@ -12,12 +12,15 @@ from datasets import load_dataset
 
 # ds = load_dataset("Riksarkivet/carl_fredrik_pahlmans_resejournaler_lines")
 
-ds = load_dataset("Riksarkivet/trolldomskommissionen_lines")
+# ds = load_dataset("Riksarkivet/trolldomskommissionen_lines")
 
-ds = load_dataset("Riksarkivet/gota_hovratt_lines")
+# ds = load_dataset("Riksarkivet/gota_hovratt_lines")
 
-ds = load_dataset("Riksarkivet/bergmastaren_i_nora_htr_lines")
+# ds = load_dataset("Riksarkivet/bergmastaren_i_nora_htr_lines")
 
-ds = load_dataset("Riksarkivet/alvsborgs_losen_lines")
+# ds = load_dataset("Riksarkivet/alvsborgs_losen_lines")
 
 # ds = load_dataset("Riksarkivet/jonkopings_radhusratt_och_magistrat_lines")
+
+### Testing Set
+ds = load_dataset("Riksarkivet/eval_htr_out_of_domain_lines")
---
+++ Riksakivet/identify_training_meta.py
@@ -0,0 +1,25 @@
+# this code is to identify characterset, max_img_line width, and max characters length 
+# in training validation and text datasets.
+
+from datasets import load_dataset
+import os
+
+
+folders =["svea_hovratt_lines",
+          "krigshovrattens_dombocker_lines",
+            "bergskollegium_relationer_och_skrivelser_lines",
+            "frihetstidens_utskottshandlingar_lines",
+            "carl_fredrik_pahlmans_resejournaler_lines",
+            "trolldomskommissionen_lines",
+            "gota_hovratt_lines",
+            "bergmastaren_i_nora_htr_lines",
+            "alvsborgs_losen_lines"
+]
+
+for folder in folders:
+    for split in ['train', 'val']:
+        ds_path = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/{split}/{folder}"
+        ds = load_from_disk(ds_path)
+        print(f"Dataset: {folder} - Split: {split}")
+        print(f"Number of examples: {len(ds)}")
+        
