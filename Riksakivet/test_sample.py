186989a056299f9915bb2b544db973e02b3d3b06
gayan68 <gayan68@yahoo.com>
2025-11-14 14:55:01 +0100

    sh

--- Riksakivet/test_sample.py
+++ Riksakivet/test_sample.py
@@ -2,7 +2,7 @@ from datasets import load_from_disk
 
 folder ="svea_hovratt_lines"
 
-train_path = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/train/{folder}"
+train_path = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/val/{folder}"
 train_ds = load_from_disk(train_path)
 
 print(train_ds)
