b7dd3f20cbb88ca92e3a77b386c66857170be1d3
gayan68 <gayan68@yahoo.com>
2025-11-14 14:37:39 +0100

    sh

--- Riksakivet/download_datasets.py
+++ Riksakivet/download_datasets.py
@@ -6,7 +6,9 @@ from datasets import load_dataset
 
 # ds = load_dataset("Riksarkivet/krigshovrattens_dombocker_lines")
 
-ds = load_dataset("Riksarkivet/bergskollegium_relationer_och_skrivelser_lines")
+# ds = load_dataset("Riksarkivet/bergskollegium_relationer_och_skrivelser_lines")
+
+ds = load_dataset("Riksarkivet/frihetstidens_utskottshandlingar_lines")
 
 # from datasets import load_dataset
 # ds = load_dataset("Riksarkivet/jonkopings_radhusratt_och_magistrat_lines")
