6da2a1d07b98231eb3037c7834589d210fb76876
gayan68 <gayan68@yahoo.com>
2025-11-14 11:53:53 +0100

    sh

--- Riksakivet/split_dataset.py
+++ Riksakivet/split_dataset.py
@@ -1,11 +1,23 @@
 from datasets import load_dataset
 
-ds = load_dataset(
+dataset  = load_dataset(
     "Riksarkivet/jonkopings_radhusratt_och_magistrat_lines",
     cache_dir="/home/x_gapat/PROJECTS/dataset_cache"
 )
 
-print(ds) 
+print(dataset)
 
-first_example = ds['train'][0]  # or ds['test'][0]
-print(first_example)
+
+full_dataset = dataset['train']
+
+# Use train_test_split with train_size=0.9
+split = full_dataset.train_test_split(test_size=0.1, seed=42)
+
+train_dataset = split['train']
+val_dataset = split['test']
+
+print(f"Train examples: {len(train_dataset)}")
+print(f"Validation examples: {len(val_dataset)}")
+
+train_dataset.save_to_disk("/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/train")
+val_dataset.save_to_disk("/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/val")
