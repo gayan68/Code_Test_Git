ac125366fc23e538e87e19e20fba302fb2ae6a0b
gayan68 <gayan68@yahoo.com>
2025-11-17 21:05:55 +0100

    sh

--- Riksakivet/split_dataset.py
+++ Riksakivet/split_dataset.py
@@ -13,40 +13,40 @@ import os
 #             "alvsborgs_losen_lines"
 # ]
 
-folders =["eval_htr_out_of_domain_lines"]
+folders =["krigshovrattens_dombocker_lines"]
 
 for folder in folders:
 
     dataset  = load_dataset(
         f"Riksarkivet/{folder}",
-        cache_dir="/home/x_gapat/PROJECTS/dataset_cache"
+        cache_dir="/home/gayapath/PROJECTS/DATA_DGX2/dataset_cache"
     )
 
     print(dataset)
 
 
-    full_dataset = dataset['test']
+    full_dataset = dataset['train']
     print(f"Total examples in full dataset: {len(full_dataset)}")
     print(full_dataset[0])
 
     # Use train_test_split with train_size=0.9
-    # split = full_dataset.train_test_split(test_size=0.1, seed=42)
+    split = full_dataset.train_test_split(test_size=0.1, seed=42)
 
-    # train_dataset = split['train']
-    # val_dataset = split['test']
+    train_dataset = split['train']
+    val_dataset = split['test']
 
-    # print(f"Train examples: {len(train_dataset)}")
-    # print(f"Validation examples: {len(val_dataset)}")
+    print(f"Train examples: {len(train_dataset)}")
+    print(f"Validation examples: {len(val_dataset)}")
 
-    # train_dir = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/train/{folder}"
-    # val_dir = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/val/{folder}"
-    test_dir = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/test/{folder}"
+    train_dir = f"/home/gayapath/PROJECTS/DATA_DGX2/Riksarkivet/train/{folder}"
+    val_dir = f"/home/gayapath/PROJECTS/DATA_DGX2//Riksarkivet/val/{folder}"
+    # test_dir = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/test/{folder}"
 
-    # os.makedirs(train_dir, exist_ok=True)
-    # os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(val_dir, exist_ok=True)
 
-    # train_dataset.save_to_disk(train_dir)
-    # val_dataset.save_to_disk(val_dir)
+    train_dataset.save_to_disk(train_dir)
+    val_dataset.save_to_disk(val_dir)
 
-    os.makedirs(test_dir, exist_ok=True)
-    full_dataset.save_to_disk(test_dir)
+    # os.makedirs(test_dir, exist_ok=True)
+    # full_dataset.save_to_disk(test_dir)
