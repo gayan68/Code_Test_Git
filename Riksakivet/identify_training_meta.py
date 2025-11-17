6272d6a15885a188cfd1e13a9ce1072aa646e66b
gayan68 <gayan68@yahoo.com>
2025-11-17 21:10:57 +0100

    sh

--- Riksakivet/identify_training_meta.py
+++ Riksakivet/identify_training_meta.py
@@ -5,21 +5,21 @@ from datasets import load_dataset, load_from_disk
 import os
 
 
-items =[{"svea_hovratt_lines": ['train', 'val']},
-          {"krigshovrattens_dombocker_lines": ['train', 'val']},
-            {"bergskollegium_relationer_och_skrivelser_lines": ['train', 'val']},
-            {"frihetstidens_utskottshandlingar_lines": ['train', 'val']},
-            {"carl_fredrik_pahlmans_resejournaler_lines": ['train', 'val']},
-            {"trolldomskommissionen_lines": ['train', 'val']},
-            {"gota_hovratt_lines": ['train', 'val']},
-            {"bergmastaren_i_nora_htr_lines": ['train', 'val']},
-            {"alvsborgs_losen_lines": ['train', 'val']}
-]
+# items =[{"svea_hovratt_lines": ['train', 'val']},
+#           {"krigshovrattens_dombocker_lines": ['train', 'val']},
+#             {"bergskollegium_relationer_och_skrivelser_lines": ['train', 'val']},
+#             {"frihetstidens_utskottshandlingar_lines": ['train', 'val']},
+#             {"carl_fredrik_pahlmans_resejournaler_lines": ['train', 'val']},
+#             {"trolldomskommissionen_lines": ['train', 'val']},
+#             {"gota_hovratt_lines": ['train', 'val']},
+#             {"bergmastaren_i_nora_htr_lines": ['train', 'val']},
+#             {"alvsborgs_losen_lines": ['train', 'val']}
+# ]
 
 #            {"eval_htr_out_of_domain_lines": ['test']}
-# items =[
-#             {"eval_htr_out_of_domain_lines": ['test']}
-# ]
+items =[
+            {"krigshovrattens_dombocker_lines": ['train', 'val']}
+]
 
 
 unique_characters = set()
@@ -29,7 +29,7 @@ max_text_length = 0
 for item in items:
     for folder_name, splits in item.items():
         for split in splits:
-            ds_path = f"/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/{split}/{folder_name}"
+            ds_path = f"/home/gayapath/PROJECTS/DATA_DGX2/Riksarkivet/{split}/{folder_name}"
             ds = load_from_disk(ds_path)
             print(f"Dataset: {folder_name} - Split: {split}")
             print(f"Number of examples: {len(ds)}")
@@ -51,7 +51,7 @@ print(f"Unique characters ({len(unique_characters)}): {''.join(sorted(unique_cha
 print(f"Max image line width: {max_img_width}") 
 print(f"Max transcription length: {max_text_length}")
 
-save_path = "/home/x_gapat/PROJECTS/DATASETS/Riksarkivet/identify_training_meta_trval.txt"
+save_path = "/home/gayapath/PROJECTS/DATA_DGX2/Riksarkivet/identify_training_meta_trval.txt"
 with open(save_path, 'w', encoding='utf-8') as f:
     f.write(f"Unique characters ({len(unique_characters)}): {''.join(sorted(unique_characters))}\n")
     f.write(f"Max image line width: {max_img_width}\n")
