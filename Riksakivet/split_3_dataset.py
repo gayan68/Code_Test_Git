a056921d146fccab8432e79717ffc8686997d788
gayan68 <gayan68@yahoo.com>
2025-11-17 21:33:08 +0100

    sh

---
+++ Riksakivet/split_3_dataset.py
@@ -0,0 +1,59 @@
+from datasets import load_dataset
+import os
+
+
+# folders =["svea_hovratt_lines",
+#           "krigshovrattens_dombocker_lines",
+#             "bergskollegium_relationer_och_skrivelser_lines",
+#             "frihetstidens_utskottshandlingar_lines",
+#             "carl_fredrik_pahlmans_resejournaler_lines",
+#             "trolldomskommissionen_lines",
+#             "gota_hovratt_lines",
+#             "bergmastaren_i_nora_htr_lines",
+#             "alvsborgs_losen_lines"
+# ]
+
+folders =["krigshovrattens_dombocker_lines"]
+
+for folder in folders:
+
+    dataset  = load_dataset(
+        f"Riksarkivet/{folder}",
+        cache_dir="/home/gayapath/PROJECTS/DATA_DGX2/dataset_cache"
+    )
+
+    print(dataset)
+
+
+    full_dataset = dataset['train']
+    print(f"Total examples in full dataset: {len(full_dataset)}")
+    print(full_dataset[0])
+
+    # Use train_test_split with train_size=0.9
+    split1 = full_dataset.train_test_split(test_size=0.3, seed=42)
+    train_dataset = split1['train']
+
+    val_test_dataset = split1['test']
+    split2 = val_test_dataset.train_test_split(test_size=0.5, seed=42)
+    val_dataset = split2['train']
+    test_dataset = split2['test']
+
+    print(f"Train examples: {len(train_dataset)}")
+    print(f"Validation examples: {len(val_dataset)}")
+    print(f"Test examples: {len(test_dataset)}")
+
+    train_dir = f"/home/gayapath/PROJECTS/DATA_DGX2/Riksarkivet/train/{folder}"
+    val_dir = f"/home/gayapath/PROJECTS/DATA_DGX2//Riksarkivet/val/{folder}"
+    test_dir = f"/home/gayapath/PROJECTS/DATA_DGX2/Riksarkivet/test/{folder}"
+
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+    
+
+    train_dataset.save_to_disk(train_dir)
+    val_dataset.save_to_disk(val_dir)
+    test_dataset.save_to_disk(test_dir)
+
+    # os.makedirs(test_dir, exist_ok=True)
+    # full_dataset.save_to_disk(test_dir)
