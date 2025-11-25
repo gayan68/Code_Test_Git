c389f0f377d38df58c807d5c69f8c574954bbf41
gayan68 <gayan68@yahoo.com>
2025-11-25 14:00:17 +0100

    sh

--- Riksakivet/download_datasets.py
+++ Riksakivet/download_datasets.py
@@ -2,25 +2,25 @@ from datasets import load_dataset
 
 ds = load_dataset("Riksarkivet/goteborgs_poliskammare_fore_1900_lines")
 
-# ds = load_dataset("Riksarkivet/svea_hovratt_lines")
+ds = load_dataset("Riksarkivet/svea_hovratt_lines")
 
-# ds = load_dataset("Riksarkivet/krigshovrattens_dombocker_lines")
+ds = load_dataset("Riksarkivet/krigshovrattens_dombocker_lines")
 
-# ds = load_dataset("Riksarkivet/bergskollegium_relationer_och_skrivelser_lines")
+ds = load_dataset("Riksarkivet/bergskollegium_relationer_och_skrivelser_lines")
 
-# ds = load_dataset("Riksarkivet/frihetstidens_utskottshandlingar_lines")
+ds = load_dataset("Riksarkivet/frihetstidens_utskottshandlingar_lines")
 
-# ds = load_dataset("Riksarkivet/carl_fredrik_pahlmans_resejournaler_lines")
+ds = load_dataset("Riksarkivet/carl_fredrik_pahlmans_resejournaler_lines")
 
-# ds = load_dataset("Riksarkivet/trolldomskommissionen_lines")
+ds = load_dataset("Riksarkivet/trolldomskommissionen_lines")
 
-# ds = load_dataset("Riksarkivet/gota_hovratt_lines")
+ds = load_dataset("Riksarkivet/gota_hovratt_lines")
 
-# ds = load_dataset("Riksarkivet/bergmastaren_i_nora_htr_lines")
+ds = load_dataset("Riksarkivet/bergmastaren_i_nora_htr_lines")
 
-# ds = load_dataset("Riksarkivet/alvsborgs_losen_lines")
+ds = load_dataset("Riksarkivet/alvsborgs_losen_lines")
 
-# ds = load_dataset("Riksarkivet/jonkopings_radhusratt_och_magistrat_lines")
+ds = load_dataset("Riksarkivet/jonkopings_radhusratt_och_magistrat_lines")
 
 ### Testing Set
-ds = load_dataset("Riksarkivet/eval_htr_out_of_domain_lines")
+# ds = load_dataset("Riksarkivet/eval_htr_out_of_domain_lines")
